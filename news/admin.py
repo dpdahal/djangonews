1bdc7e904a818b2674da2d84a164f5e596a3c433
dpdahal <dp48dahal@gmail.com>
2024-10-30 08:54:22 +0545

    update

--- news/admin.py
+++ news/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Student
+
+# admin.site.register(Student)
+
+@admin.register(Student)
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ['name', 'email', 'phone', 'address']
+   
--- news/models.py
+++ news/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Student(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    address = models.CharField(max_length=200)
+    phone = models.CharField(max_length=15)
+
